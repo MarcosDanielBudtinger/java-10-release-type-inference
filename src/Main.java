fb86a23a73ad220eb1d05e9d0647d9351639481b
Marcos Daniel <marcos.mdbbr@gmail.com>
2021-10-18 23:00:52 -0300

    utilizando var

--- src/Main.java
+++ src/Main.java
@@ -8,12 +8,61 @@ import java.util.stream.Collectors;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        URL url = new URL("https://docs.oracle.com/javase/10/language");
 
-        URLConnection conn = url.openConnection();
+//        URL url = new URL("https://docs.oracle.com/javase/10/language");
+//
+//        URLConnection conn = url.openConnection();
+//
+//        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+//
+//        System.out.println(bufferedReader.lines().collect(Collectors.joining()).replaceAll(">", ">\n"));
 
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        withInference();
+        testar("Marcos Daniel");
+        printarSoma(10, 5, 5);
+        withInferenceTwo();
+    }
+
+    //var só pode ser utilizado a nível de método, não pode ser utilizado a nível de classe e nem como parametro
+    public static void withInference () throws IOException {
+
+        var url = new URL("https://docs.oracle.com/javase/10/language");
+
+        var conn = url.openConnection();
+
+        var bufferedReader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
 
         System.out.println(bufferedReader.lines().collect(Collectors.joining()).replaceAll(">", ">\n"));
+    }
+
+    //var só pode ser utilizado a nível de método, não pode ser utilizado a nível de classe e nem como parametro
+    public static void withInferenceTwo () throws IOException {
+
+        var url = new URL("https://docs.oracle.com/javase/10/language");
+        var conn = url.openConnection();
+
+        try (var bufferedReader = new BufferedReader(new InputStreamReader(conn.getInputStream()));) {
+            System.out.println(bufferedReader.lines().collect(Collectors.joining()).replaceAll(">", ">\n"));
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void testar(String nome) {
+
+        var nomeCompleto = new StringBuilder().append(nome)
+                .append(" ").append("Budtinger");
+        System.out.println(nomeCompleto);
+    }
+
+    public static void printarSoma(int... numeros) {
+
+        var soma = 0;
+        if (numeros.length > 0) {
+            for (var numero: numeros) {
+                soma += numero;
+            }
+            System.out.println("A soma é : " + soma);
+        }
     }
 }
